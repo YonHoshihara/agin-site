7a87f2ddd5d0b714cda0dd248df15b1d43939d34
YonHoshihara <hoshiharyon@gmail.com>
2020-03-12 00:10:19 -0400

    add caurosel to Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'agin-site';
-  infos = [{"a":1}, {"a":2}];
+  infos = [{"a":1}, {"a":2}, {"a":3}];
+  map_mundi = "https://quadrosincriveis.com.br/wp-content/uploads/2019/09/qi0610_MapaMundi_black_A3_FLT_preta.png";
+  image_1= "https://observatorio3setor.org.br/wp-content/uploads/2016/08/reprograma.jpg";
+  image_2 = "https://www.eusoudev.com.br/wp-content/uploads/2018/11/women-who-code.png";
+  image_3 = "https://blog.locaweb.com.br/wp-content/uploads/2017/03/post_blog-dia-da-mulher.png";
 }
 
